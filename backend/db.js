2984b7a0763278ad3f61746053a0165278f9ea69
abec1214 <rfr38t4p@icloud.com>
2026-08-15 02:04:45 +0900

    fix route path

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "Z86c4rpP",
-  database: "graduation_app",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 module.exports = db;
